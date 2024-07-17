89c0d88c1689a69109785c38496a61f363116992
vanshika705 <vnshkjngd@gmail.com>
2024-07-17 21:57:53 +0530

    data science

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template,request
+from flask import Flask, render_template,request,url_for
 # ['age', 'sex', 'bmi', 'children', 'smoker', 'weight', 'region_northeast','region_northwest', 'region_southeast', 'region_southwest']
 
 import joblib
@@ -11,6 +11,29 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    if request.method=="POST":
+        age = request.form["age"]
+        sex =request.form["sex"]
+        bmi =request.form["bmi"] 
+        children =request.form["children"] 
+        smoker =request.form["smoker"] 
+        weight =request.form["weight"] 
+        region =request.form["region"] 
+        
+        
+    user_data = [[age, sex, bmi, children, smoker, weight, region]]
+    return user_data
+        
+        
+        
+         
 
 if __name__ == "__main__":
     app.run(debug=True)
